37ff355c3d7fb05652cc09bed5ff6f9be45c6202
jicjjang <jicjjang12@gmail.com>
2017-08-27 00:58:43 +0900

    append facebook middleware what about authentication.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 import express from 'express'
+import morgan from 'morgan'
+import fs from 'fs'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 import index from './routes/index'
 
 const app = express()
 const port = 3000
 
+// config - logs, cookie, cors, route
+app.use(morgan('combined', {stream: fs.createWriteStream(path.join(__dirname + '/../logs', 'access.log'), {flags: 'a'})}))
+app.use(cookieParser())
+app.use(cors())
 app.use('/', index)
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send({
+    code: err.status || 500,
+    message: err.message || 'Server error'
+  })
+})
+
 
 const server = app.listen(port, () => {
   console.log('Express listening on port', port)
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,28 @@
 import express from 'express'
+import axios from 'axios'
 
 const router = express.Router()
 
 router.use((req, res, next) => {
-  // somthing to do. like, login check...
-  console.log('Requested path: %s', req.path)
-  next()
+  const keyList = Object.keys(req.query)
+  if (keyList.length > 0 && keyList.includes('access_token') && keyList.includes('id') && keyList.includes('platform')) {
+    axios.get('https://graph.facebook.com/me?fields=id,name,picture&access_token=' + req.query.access_token).then(res => {
+      if (res.data.id === req.query.id) {
+        next()
+      }
+    }).catch(function(err) {
+      let errDetail = new Error('You didn\'t have authentication.')
+      errDetail.status = 401
+      return next(errDetail)
+    })
+  }
 })
 
 router.use('/', express.static(__dirname + '/../../'))
+router.use('/static', express.static(__dirname + '/../../feeds'))
 
 router.get('/:id', (req, res) => {
-  res.send('world id : ' + req.params.id)
+  res.send('world id : ' + req.params.id + ' ' + req.query.test)
 })
 
 export default router
