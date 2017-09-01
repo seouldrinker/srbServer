1b2c0a99a70c68d702e527f7b0f75bd6801ca117
jicjjang <jicjjang12@gmail.com>
2017-09-01 23:28:58 +0900

    modified import file path

--- server/routes/explore.js
+++ server/routes/explore.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import axios from 'axios'
 import { authentication } from '../middleware/authentication'
-import { getFilteredAllWalkCourse, getOneWalkCourse } from '../middleware/explore'
+import { getFilteredAllWalkCourse, getOneWalkCourse } from '../modules/explore'
 
 const router = express.Router()
 
@@ -21,6 +21,9 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   const results = await getOneWalkCourse(encodeURIComponent(req.params.id), next)
+
+  // 유저들이 올린 사진 필요 (CPI_IDX 값을 기준으로.)
+
   if (typeof results !== 'undefined') {
     res.send({
       code: 200,
@@ -32,6 +35,7 @@ router.get('/:id', async (req, res, next) => {
 })
 
 export default router
+
 
 // clear      : authentication                    // 공통 middleware. 인증
 //            : /srb/vbeta/explore?page=2         // 서울시 API를 읽어와서 둘레길 리스트 뿌려주기. 사용자가 올린 길 경로들에 대해서 처음 20개를 제공
